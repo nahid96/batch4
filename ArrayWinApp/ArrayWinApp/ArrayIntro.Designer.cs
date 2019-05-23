24853c4dbfc4ffd5d11eb4080ab5608f0754e987
nahid96 <nahid.nawaz96@gmail.com>
2019-05-23 13:50:39 +0600

    day3 class task

---
+++ ArrayWinApp/ArrayWinApp/ArrayIntro.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace ArrayWinApp
+{
+    partial class ArrayIntro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ResultRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.ShowButton = new System.Windows.Forms.Button();
+            this.addTextBox = new System.Windows.Forms.TextBox();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.ReverseButton = new System.Windows.Forms.Button();
+            this.SumButton = new System.Windows.Forms.Button();
+            this.CopyButton = new System.Windows.Forms.Button();
+            this.DuplicteButton = new System.Windows.Forms.Button();
+            this.Uniquebutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ResultRichTextBox
+            // 
+            this.ResultRichTextBox.Location = new System.Drawing.Point(31, 48);
+            this.ResultRichTextBox.Name = "ResultRichTextBox";
+            this.ResultRichTextBox.Size = new System.Drawing.Size(275, 351);
+            this.ResultRichTextBox.TabIndex = 0;
+            this.ResultRichTextBox.Text = "";
+            // 
+            // ShowButton
+            // 
+            this.ShowButton.Location = new System.Drawing.Point(366, 66);
+            this.ShowButton.Name = "ShowButton";
+            this.ShowButton.Size = new System.Drawing.Size(75, 23);
+            this.ShowButton.TabIndex = 1;
+            this.ShowButton.Text = "Show";
+            this.ShowButton.UseVisualStyleBackColor = true;
+            this.ShowButton.Click += new System.EventHandler(this.ShowButton_Click);
+            // 
+            // addTextBox
+            // 
+            this.addTextBox.Location = new System.Drawing.Point(56, 22);
+            this.addTextBox.Name = "addTextBox";
+            this.addTextBox.Size = new System.Drawing.Size(100, 20);
+            this.addTextBox.TabIndex = 2;
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(231, 22);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(75, 23);
+            this.AddButton.TabIndex = 3;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // ReverseButton
+            // 
+            this.ReverseButton.Location = new System.Drawing.Point(371, 115);
+            this.ReverseButton.Name = "ReverseButton";
+            this.ReverseButton.Size = new System.Drawing.Size(75, 23);
+            this.ReverseButton.TabIndex = 4;
+            this.ReverseButton.Text = "Reverse";
+            this.ReverseButton.UseVisualStyleBackColor = true;
+            this.ReverseButton.Click += new System.EventHandler(this.ReverseButton_Click);
+            // 
+            // SumButton
+            // 
+            this.SumButton.Location = new System.Drawing.Point(371, 175);
+            this.SumButton.Name = "SumButton";
+            this.SumButton.Size = new System.Drawing.Size(75, 23);
+            this.SumButton.TabIndex = 5;
+            this.SumButton.Text = "Sum";
+            this.SumButton.UseVisualStyleBackColor = true;
+            this.SumButton.Click += new System.EventHandler(this.SumButton_Click);
+            // 
+            // CopyButton
+            // 
+            this.CopyButton.Location = new System.Drawing.Point(371, 232);
+            this.CopyButton.Name = "CopyButton";
+            this.CopyButton.Size = new System.Drawing.Size(75, 23);
+            this.CopyButton.TabIndex = 6;
+            this.CopyButton.Text = "Copy";
+            this.CopyButton.UseVisualStyleBackColor = true;
+            this.CopyButton.Click += new System.EventHandler(this.CopyButton_Click);
+            // 
+            // DuplicteButton
+            // 
+            this.DuplicteButton.Location = new System.Drawing.Point(371, 289);
+            this.DuplicteButton.Name = "DuplicteButton";
+            this.DuplicteButton.Size = new System.Drawing.Size(75, 23);
+            this.DuplicteButton.TabIndex = 7;
+            this.DuplicteButton.Text = "Duplicate";
+            this.DuplicteButton.UseVisualStyleBackColor = true;
+            this.DuplicteButton.Click += new System.EventHandler(this.DuplicteButton_Click);
+            // 
+            // Uniquebutton
+            // 
+            this.Uniquebutton.Location = new System.Drawing.Point(371, 340);
+            this.Uniquebutton.Name = "Uniquebutton";
+            this.Uniquebutton.Size = new System.Drawing.Size(75, 23);
+            this.Uniquebutton.TabIndex = 8;
+            this.Uniquebutton.Text = "Unique";
+            this.Uniquebutton.UseVisualStyleBackColor = true;
+            this.Uniquebutton.Click += new System.EventHandler(this.Uniquebutton_Click);
+            // 
+            // ArrayIntro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Uniquebutton);
+            this.Controls.Add(this.DuplicteButton);
+            this.Controls.Add(this.CopyButton);
+            this.Controls.Add(this.SumButton);
+            this.Controls.Add(this.ReverseButton);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.addTextBox);
+            this.Controls.Add(this.ShowButton);
+            this.Controls.Add(this.ResultRichTextBox);
+            this.Name = "ArrayIntro";
+            this.Text = "Array Intro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox ResultRichTextBox;
+        private System.Windows.Forms.Button ShowButton;
+        private System.Windows.Forms.TextBox addTextBox;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button ReverseButton;
+        private System.Windows.Forms.Button SumButton;
+        private System.Windows.Forms.Button CopyButton;
+        private System.Windows.Forms.Button DuplicteButton;
+        private System.Windows.Forms.Button Uniquebutton;
+    }
+}
+
---
+++ ArrayWinApp/ArrayWinApp/ArrayIntro.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArrayWinApp
+{
+    public partial class ArrayIntro : Form
+    {
+        const int size = 10;
+        int index = 0;
+        int[] numbers = new int[size];
+
+        public ArrayIntro()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            
+
+            string message = "";
+
+            for(int index=0; index<size; index++)
+            {
+                if (numbers[index] != 0)
+                    message = message + "Value of Index [" + index + "] is " + numbers[index].ToString() + "\n"; 
+            }
+
+            ResultRichTextBox.Text = message;
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            numbers[index] = Convert.ToInt32(addTextBox.Text);
+            index++;
+            addTextBox.Text = string.Empty;
+        }
+
+        private void ReverseButton_Click(object sender, EventArgs e)
+        {
+            ResultRichTextBox.Text = string.Empty;
+
+            string message = "";
+
+            for (int index = size-1; index >= 0; index--)
+            {
+                if (numbers[index] != 0)
+                    message = message + "Value of Index [" + index + "] is " + numbers[index].ToString() + "\n";
+            }
+
+            ResultRichTextBox.Text = message;
+        }
+
+        private void SumButton_Click(object sender, EventArgs e)
+        {
+            int sum = 0;
+
+            for(int index = 0; index < size; index++)
+            {
+                if (numbers[index] != 0)
+                    sum = sum + numbers[index];
+            }
+
+            ResultRichTextBox.Text = "Sum is : " + sum.ToString();
+
+        }
+
+        private void CopyButton_Click(object sender, EventArgs e)
+        {
+            int[] reverseArray = new int[size];
+
+            for (int index = 0; index < size; index++)
+            {
+                reverseArray[index] = numbers[index];
+            }
+
+            ResultRichTextBox.Text = string.Empty;
+
+            string message = "";
+
+            for (int index = 0; index < size; index++)
+            {
+                if (reverseArray[index] != 0)
+                    message = message + "Value of Index [" + index + "] is " + reverseArray[index].ToString() + "\n";
+            }
+
+            ResultRichTextBox.Text = message;
+        }
+
+        private void DuplicteButton_Click(object sender, EventArgs e)
+        {
+            int[] duplicateArray = new int[size];
+            int count = 0;
+            for (int index=0; index<size; index++)
+            {
+                int flag = 0;
+                
+                for(int check=index+1; check<size; check++)
+                {
+                    if (numbers[index] == numbers[check])
+                        flag = numbers[index];
+                }
+                if(flag != 0)
+                {
+                    duplicateArray[count] = flag;
+                    count++;
+                }
+                    
+            }
+
+            string message = "";
+
+            for (int index = 0; index < size; index++)
+            {
+                if (duplicateArray[index] != 0)
+                    message = message + "Value of Index [" + index + "] is " + duplicateArray[index].ToString() + "\n";
+            }
+
+            ResultRichTextBox.Text = message;
+
+        }
+
+        private void Uniquebutton_Click(object sender, EventArgs e)
+        {
+            int[] uniqueArray = new int[size];
+            int count = 0;
+            for (int index = 0; index < size; index++)
+            {
+                int flag = 0;
+
+                for (int check = index + 1; check < size; check++)
+                {
+                    if (numbers[index] != numbers[check])
+                        flag = numbers[index];
+                }
+                if (flag != 0)
+                {
+                    uniqueArray[count] = flag;
+                    count++;
+                }
+
+            }
+
+            string message = "";
+
+            for (int index = 0; index < size; index++)
+            {
+                if (uniqueArray[index] != 0)
+                    message = message + "Value of Index [" + index + "] is " + uniqueArray[index].ToString() + "\n";
+            }
+
+            ResultRichTextBox.Text = message;
+        }
+    }
+}
